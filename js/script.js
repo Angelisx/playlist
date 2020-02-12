00e89d245d6084aed5d91f8e4bf51b2b6b58ef31
Angelisx <bladetax132@gmail.com>
2020-02-12 23:27:50 +0000

    finish the java man

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-let 
+let inputs = [
+        {   song: "ggbro" ,
+            artist:"ffgff" ,
+            length:"fnifn" ,
+            image:"ytdc",
+            sUrl:"eknef"
+        }
+    ];
+
+$("#add").click(function(){
+    let song = $("#songName").val();
+    let artist = $("#artistName").val();
+    let length = $("#songLength").val();
+    let image = $("#imgUrl").val();
+    let sUrl = $("#songUrl").val();
+    
+    let somgd =  {song, artist , length , image, sUrl};
+
+    inputs.push(somgd);
+    
+    inputs.forEach(function(songs) {
+        $(".cart").append(songs.song);
+
+        console.log(songs.song)
+        console.log(songs.artist)
+
+    });
+
+    console.log(inputs);
+
+//        $("#cart").append("<li>"+songs.song+"</li>");
+
+
+});
+
+
+
+
